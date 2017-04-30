cdc8fbcde62329de766561faa6ce63a82b9c311b
1caruxx <fernandohugolareu@hotmail.com>
2017-04-30 20:34:04 -0300

    Apuntes de programacion
    Se han aniadido en un repositorio los apuntes del campus virtual de la materia laboratorio 1 - programacion 1 y laboratorio 2 - programacion 2.

---
+++ 1° cuatrimestre/Clase 1/Ejemplo1/main.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+
+
+int main()
+{
+    float valorA;
+    float valorB;
+    float resultado;
+    char letra;
+  double j=sqrt(16);
+    printf("Ingrese el primer valor: ");
+    scanf("%f", &valorA);
+
+    printf("Ingrese el segundo valor: ");
+    scanf("%f", &valorB);
+
+  printf("----%lf", j);
+
+
+    resultado=valorA+valorB;
+
+        printf("Ingrese el primer valor: ");
+    scanf("%f", &valorA);
+
+
+    printf("Hello world!\n");
+
+    printf("El resultado es: %d \n", &resultado);
+   // printf("la letra es: %c \n", letra);
+
+   // alert("el resultado es: " + resultado + " y la letra es: "+letra);
+
+    //printf("La suma entre %f y %f es: %f y la letra es: %c",valorA,valorB, resultado, letra);
+
+
+
+
+
+
+    return 0;
+}
